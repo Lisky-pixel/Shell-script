4a9bcaeaa1d5daa009463519af75d94a8399146b
Amandine0610 <a.irakoze@alustudent.com>
2023-10-09 03:11:18 -0700

    select-emails.sh

---
+++ select-emails.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Input file containing student information
+input_file="Students-list_0923.txt"
+
+# Regular expression to match email addresses
+email_pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}"
+
+# Use grep with the -E (extended regex) option to extract emails
+grep -E -o "$email_pattern" "$input_file" > student_emails.txt
+
+echo "Student email addresses extracted and saved to student_emails.txt"
