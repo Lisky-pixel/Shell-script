5c4a437c3150de9f5ae1cd97a9fe8d7f7a664332
KhotKeys <gabrielkhotgarangpawuoi@gmail.com>
2023-10-13 02:22:43 -0700

    What does friends do?

--- student_records/move-to-directory.sh
+++ student_records/move-to-directory.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Define the directory name
+
 directory_name="negpod_16-q1"
 
-# Create the directory if it doesn't exist
+
 if [ ! -d "$directory_name" ]; then
     mkdir "$directory_name"
 fi
 
-# Move the files to the directory
 mv main.sh student_emails.txt select-emails.sh students-list_0923.txt "$directory_name"
 
 echo "Files moved to $directory_name."
